e21c8497663b9159b29343540a802c22b2a9378d
alihaider <chauhdryali15@gmail.com>
2021-05-01 22:30:25 +0500

    Still Stuck in CSS
    Continue

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log('Hello');
+
+switch (6) {
+    case 0:
+        Text='Weekend';
+        break;
+    case 6:
+        Text="Weekday";
+        break;
+    default:
+        Text="Nothing";
+        break;
+}
+console.log(Text);
